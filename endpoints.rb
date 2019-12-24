28d76f3519092cf6048528cc2139f17bbabb089f
okolesnichenko <okolesnichenko@phoenixit.ru>
2019-12-24 17:40:13 +0300

    mockup v1.0.1

--- endpoints.rb
+++ endpoints.rb
@@ -10,6 +10,10 @@ class Endpoints < Sinatra::Base
     content_type 'application/json'
   end
 
+  not_found do
+    {'status': 404}.to_json
+  end
+
   redis = Redis.new(:host => '127.0.0.1', :port => '6380')
   repository = Repository.new(redis)
 
@@ -20,4 +24,22 @@ class Endpoints < Sinatra::Base
   post "/endpoints" do
     repository.post(request.body.read)
   end
+
+  delete "/endpoints/:path" do |path|
+    repository.delete(path)
+  end
+
+  get "/:path" do |path|
+    repository.check(path, request.request_method.downcase)
+  end
+  post "/*" do |path|
+    repository.check(path, request.request_method.downcase)
+  end
+  put "/*" do |path|
+    repository.check(path, request.request_method.downcase)
+  end
+  delete "/*" do |path|
+    repository.check(path, request.request_method.downcase)
+  end
+
 end
--- repository.rb
+++ repository.rb
@@ -4,7 +4,11 @@ class Repository
   end
 
   def get
-    response_array = @redis.mget(@redis.keys('*'))
+    keys = @redis.keys('*')
+    if keys.empty?
+      raise Sinatra::NotFound
+    end
+    response_array = @redis.mget(keys)
     JSON.parse response_array.to_json
   end
 
@@ -12,7 +16,40 @@ class Repository
     unless body['method'] && body['path'] && body['status'] && body['payload']
       return {'message': 'Can not create mock entity.'}
     end
-    @redis.set body['path'], body
+    path = JSON.parse(body)['path']
+    @redis.set path, body
+  end
+
+  def delete(path)
+    req = @redis.del path
+    if req == 0
+      raise Sinatra::NotFound
+    else
+      'ok'
+    end
+  end
+
+  def check(path, method)
+
+    unless @redis.keys("*").include? path
+      raise Sinatra::NotFound
+    end
+    response = JSON.parse(@redis.get path)
+    unless response && response["method"].downcase == method.downcase
+      raise Sinatra::NotFound
+    end
+    case method.downcase
+    when 'get'
+      {'payload':response['payload']}.to_json
+    when 'post'
+      {'status':response["status"]}.to_json
+    when 'put'
+      {'status':response["status"]}.to_json
+    when 'delete'
+      {'status':response["status"]}.to_json
+    else
+      {'status':response["status"], 'payload':response['payload']}.to_json
+    end
   end
 
 end
