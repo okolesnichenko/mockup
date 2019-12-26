b3163ab60875cbff433e79c56b219c8a2c3e994c
okolesnichenko <okolesnichenko@phoenixit.ru>
2019-12-26 15:45:40 +0300

    right tests

--- spec/app/endpoitns_spec.rb
+++ spec/app/endpoitns_spec.rb
@@ -1,14 +1,32 @@
 require_relative "../../spec/spec_helper"
-
+require 'json'
 
 describe Endpoints do
-  context "all mock endpoints" do
+  context "GET/POST to endpoints" do
     it "returns endpoint" do
       get "/endpoints"
       last_response.ok?
     end
     it "returns 200" do
       get "/endpoints"
+      expect(last_response.status).to eq 200
+    end
+
+    it "add posts mock to app" do
+      post "/endpoints", {
+          method: "GET",
+          path: "posts",
+          status: 200,
+          payload: {
+              name: "Golden fish",
+              pages: 123
+          }
+      }.to_json, format: :json
+      expect(last_response.body).to eq "OK"
+    end
+
+    it "check posts module" do
+      get "/posts"
       expect(last_response.status).to eq 200
     end
   end
